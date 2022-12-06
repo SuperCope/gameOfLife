380a3523847d35f7d5d54484a08042f7caa0867c
hugogoyard <goyardh@gmail.com>
2022-12-06 12:02:26 +0100

    Edit the GUI

--- src/Cellule.java
+++ src/Cellule.java
@@ -1,5 +1,3 @@
-import javafx.scene.control.Cell;
-
 import java.io.Serializable;
 
 public class Cellule implements CelluleInterface, Serializable {
--- src/GUI.java
+++ src/GUI.java
@@ -1,28 +1,69 @@
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+//import java.awt.event.*;
 
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
+public class GUI extends JFrame
+{
+    JButton[][] cellsGrid;
+    JPanel gridPanel;
 
-public class GUI {
-    public GUI() {
+    String balls[] = {
+        "redBall.png",
+        "blueBall.png",
+        "redBall.png"
+    }
+    
+    JButton cell;
 
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+    private int width = 700;
+    private int height = width;
 
-        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 10, 300));
-        panel.setLayout(new GridLayout(0, 1));
+    public GUI() 
+    {
+        //  Windows Content
+        newView();
 
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("J3U 2 14 V13");
-        frame.pack();
-        frame.setVisible(true);
-        frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        // Windows Specs
+        setSize(width, height);
+        setTitle("Jeu de la vie - Hugo GOYARD & Robin VUITTENEZ");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+        setDisplay(true);   
     }
 
-    public static void main(String[] args) {
-        new GUI();
+    public void newView()
+    {
+        cellsGrid = new JButton[100][100];
+        gridPanel = new JPanel(new GridLayout(100,100));
+        gridPanel.setBackground(Color.WHITE);
+        
+        makeGrid();
+    }
+
+    public void makeGrid()
+    {
+        for (int i = 0; i < cellsGrid.length; i++) {
+            for (int j = 0; j < cellsGrid[i].length; j++) {
+                cell = new JButton(new ImageIcon(balls[0]));
+                cell.setEnabled(false);
+                cell.setBorder(null);
+                cell.setBackground(Color.WHITE);
+                cell.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.GRAY));
+                cell.setPreferredSize(new Dimension(width/100, height/100));
+                gridPanel.add(cell);
+                cellsGrid[i][j] = cell;
+            }
+        }
+        add(gridPanel);
+    }
+
+    public void setDisplay(Boolean value)
+    {
+        setVisible(value);
+    }
+
+
+    public static void main(String[] args)
+    {
+        GUI gui = new GUI();
     }
 }
